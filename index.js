0c3e572d4cc3d557b8054e2a2d5a20e68a2aa614
Joshua M. Svenson <joshua-svenson@uiowa.edu>
2017-03-01 23:27:03 -0600

    improved layout, added Emily Blunt episode, added button to go to next guest instead of all on one page

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function openCity(evt, cityName) {
+function openNetwork(evt, networkName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
@@ -15,6 +15,23 @@ function openCity(evt, cityName) {
     }
 
     // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(networkName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function changeGuest(evt) {
+	var Box_of_Lies_games
+	
+	Box_of_Lies_games = document.getElementsByClassName("Box_of_Lies_games");
+	
+    for (i = 0; i < Box_of_Lies_games.length; i++) {
+		if(Box_of_Lies_games[i].style.display !== "none" && i+1 < Box_of_Lies_games.length){
+			Box_of_Lies_games[i].style.display = "none";
+			Box_of_Lies_games[i+1].style.display = "block";
+			break;
+		}
+		else{
+			Box_of_Lies_games[i].style.display = "none";
+		}
+    }	
 }
